02f7ea79814833658e0d827e56f266ae850bfb35
Fredy Velásquez <macacoloco696@gmail.com>
2024-05-17 12:28:12 -0600

    advanced hooks

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,16 +3,33 @@
 
 import * as React from 'react'
 
+ //accepts the current state and the action (dispatch) this is the "setSomething" stuff
+//  const countReducer = (state, action) => ({
+//   ...state,
+//   ...(typeof action === 'function' ? action(state): action),
+// })
+
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + action.step
+      }
+  
+    default:
+      break;
+  }
+}
+
 function Counter({initialCount = 0, step = 1}) {
  
-  //accepts the current state and the action (dispatch) this is the "setSomething" stuff
-  const countReducer = (count, step) => {
-    return count + step;
-  }
 
   //receives a function and a initialValue
-  const [count, changeCount] = React.useReducer(countReducer, initialCount)
-  const increment = () => changeCount(step)
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
